82fa61d3bfc64175a863d3dad08cf0cf5c90c688
ohdwjs <Jeon@DESKTOP-VR2NNII>
2016-04-19 08:48:29 +0900

    quizzzzzz

--- papajohns/src/main/java/papa/quizAnswer/model/quizAnswerDAO.java
+++ papajohns/src/main/java/papa/quizAnswer/model/quizAnswerDAO.java
@@ -1,5 +1,6 @@
 package papa.quizAnswer.model;
-
+import java.util.*;
 public interface quizAnswerDAO {
 	public int quizAnswer(quizAnswerDTO dto);
+	public List<quizAnswerDTO> quizResult(int idx);
 }
